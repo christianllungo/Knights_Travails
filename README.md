427d26ce1ff8664e0f4567aa9d1241cbd1f99ecf
Christian Llungo <christianllungo9@gmail.com>
2019-05-17 18:28:02 -0700

    Phase 1 of PolyTreeNode completed

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Knights Travails Project
 ========
 
+
 Documentation to be updated.
--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,6 +1,33 @@
+
 # This class represents a node in a tree
 # The tree can have multiple children unlike binary tree (left, right)
 
 class PolyTreeNode
 
+    # constructor. Sets value of node
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    # returns parent of the node
+    def parent
+        @parent
+    end
+
+    # returns array of children of the node
+    def children
+        @children
+    end
+
+    # returns the value of the node
+    def value
+        @value
+    end
+
+    def parent=(parent)
+        @parent = parent
+        parent.children << self unless parent == nil
+    end
 end
