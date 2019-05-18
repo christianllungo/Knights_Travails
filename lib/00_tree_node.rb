4596534d517d7c74684a6b4c464f6e5cb9823dcf
Christian Llungo <christianllungo9@gmail.com>
2019-05-17 19:19:11 -0700

    Phase 3 of PolyTreeNode completed

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -26,12 +26,25 @@ class PolyTreeNode
         @value
     end
 
-    # sets parent node unless it's the root node
     # removes node from old parents array of children
+    # sets the node's new parent 
+    # adds the node to the new parent's array of children
     def parent=(parent)
         @parent.children.delete(self) if @parent != nil
         @parent = parent
         parent.children << self unless parent == nil
+    end
+
+    # adds this node to child_node @parent
+    def add_child(child_node)
+        child_node.parent=(self)
+    end
+
+    # sets nil to child_node @parent
+    def remove_child(child_node)
+        raise "Error: Not a child!" if !@children.include?(child_node)
+        child_node.parent=(nil)
+
     end
 
     
